7c443eb4b333b04d9c944400e249f2b0abfe35a2
Drostaei <ost1219@gmail.com>
2025-03-18 03:05:47 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,14 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+apiKey: "AIzaSyBOk1mlB6ZBmgeSd2-zR32tnAVw-5I0W_0",
+  authDomain: "jtwedding-98fcc.firebaseapp.com",
+  databaseURL: "https://jtwedding-98fcc-default-rtdb.firebaseio.com",
+  projectId: "jtwedding-98fcc",
+  storageBucket: "jtwedding-98fcc.firebasestorage.app",
+  messagingSenderId: "338679064088",
+  appId: "1:338679064088:web:c944758a426543522fc56d",
+  measurementId: "G-5CJK2YFY41"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
