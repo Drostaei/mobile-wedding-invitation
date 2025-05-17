77c5c057929813f6f3713f2e4fc5dabf88e808e4
Drostaei <ost1219@gmail.com>
2025-05-17 21:33:13 +0900

    Update Invitation.tsx

--- src/layout/Invitation/Invitation.tsx
+++ src/layout/Invitation/Invitation.tsx
@@ -4,6 +4,22 @@ import Host from '../Contact/Host.tsx';
 import RoundButton from '@/components/RoundButton.tsx';
 import { Caption, Paragraph } from '@/components/Text.tsx';
 
+const Invitation = () => {
+  const { greeting } = data;
+  return (
+    <InvitationWrapper>
+      <Paragraph>{greeting.message}</Paragraph>
+      <Host />
+      <Caption textAlign={'center'}>{greeting.eventDetail}</Caption>
+      {/* TODO: 구글캘린더 추가하기 기능을 넣는다면 링크 수정 */}
+      <RoundButton
+        target="_blank"
+        href=""
+        rel="noreferrer">
+      </RoundButton>
+    </InvitationWrapper>
+  );
+};
 
 export default Invitation;
 
